f87514e0133ba99891279e5252ad36582735cf71
william xyz <wiltang256@gmail.com>
2025-05-04 19:06:12 -0300

    Setup iOS Koin modules

--- composeApp/src/iosMain/kotlin/com/example/kmprandomanime/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/kmprandomanime/MainViewController.kt
@@ -1,5 +1,25 @@
 package com.example.kmprandomanime
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.example.kmprandomanime.di.appModule
+import com.example.kmprandomanime.di.dataModule
+import com.example.kmprandomanime.di.interactorModule
+import com.example.kmprandomanime.di.networkModule
+import com.example.kmprandomanime.di.platformModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        startKoin {
+            modules(
+                appModule,
+                dataModule,
+                networkModule,
+                interactorModule,
+                platformModule(),
+            )
+        }
+    }
+) {
+    App()
+}
